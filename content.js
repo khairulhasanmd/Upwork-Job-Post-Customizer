2f30ed9d63881bcf4085b4b9d1c191f5a7ad1d4a
Md. Khairul Hasan <khairulhasanmd@gmail.com>
2022-03-13 01:07:08 +0600

    new code

--- content.js
+++ content.js
@@ -1,20 +1,22 @@
+
 setInterval(mainOperation, 3000);
 
 var blockedCountries = ["India", "Pakistan", "Bangladesh", "Israel"];
 var blockedCountriesLength = blockedCountries.length;
 
 function mainOperation() {
-    var countriesOfJobPosts = document.querySelectorAll('.d-none > strong');
-    var len = countriesOfJobPosts.length;
+    var jobPosts = document.querySelectorAll('.up-card-list-section');
+    var len = jobPosts.length;
     for (var i = 0; i < len; i++) {
-        if (countriesOfJobPosts[i].parentElement.parentElement.previousSibling.previousSibling.childNodes[0].childNodes[2].innerText == "Less than 5"){//bid count
-            countriesOfJobPosts[i].parentElement.parentElement.parentElement.parentElement.style.backgroundColor = "#6fda44";
+        if (jobPosts[i].querySelector('[data-test="proposals"]').innerText == "Less than 5"){//bid count
+            jobPosts[i].style.backgroundColor = "#6fda44";
         }
         for (var j = 0; j < blockedCountriesLength; j++) {
-            if (countriesOfJobPosts[i].innerText == blockedCountries[j]){//country
-                countriesOfJobPosts[i].parentElement.parentElement.parentElement.parentElement.style.backgroundColor = "silver";
+            if (jobPosts[i].querySelector('.d-none > strong').innerText == blockedCountries[j]){//country
+                jobPosts[i].style.backgroundColor = "silver";
             }
         }
     }
 }
 
+document.querySelectorAll('.up-card-list-section')
